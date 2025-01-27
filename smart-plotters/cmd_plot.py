fixed masking

smart-plotters/cmd_plot.py
@@ -61,9 +61,15 @@ class Plotter:
 
     def plot_contour_CCD(self, band1, band2, band3, band4, ax=None, bins=100, threshold=5, cmap='autumn_r', color='k', s=1):
         import mpl_plot_templates as template
-        x = np.array(self.color(band1, band2))
+        #x = np.array(self.color(band1, band2))
+        x = self.color(band1, band2)
+        x[x.mask] = np.nan
+        x = np.array(x)
         #x[x>5] = np.nan
-        y = np.array(self.color(band3, band4))
+        #y = np.array(self.color(band3, band4))
+        y = self.color(band3, band4)
+        y[y.mask] = np.nan
+        y = np.array(y)
         #y[y>5] = np.nan
         if ax is None:
             ax = plt.subplot()